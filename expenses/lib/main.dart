6abe0ace2df4e0eeca7b39922e458483b960a7e0
Alessandro Kennedy <juniorkennedy15@gmail.com>
2021-07-19 21:31:20 -0300

    passando id pra formato uuid

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -78,19 +78,33 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final appBar = AppBar(
+        title: Text('Despesas Pessoais',
+            style: TextStyle(
+                fontSize: 20 * MediaQuery.of(context).textScaleFactor)),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _openTransactionFormModal(context),
+          ),
+        ]);
+    final avaliableHeight = MediaQuery.of(context).size.height -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
     return Scaffold(
-      appBar: AppBar(title: Text('Despesas Pessoais'), actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.add),
-          onPressed: () => _openTransactionFormModal(context),
-        ),
-      ]),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Chart(_recentTransactions),
-            TransactionList(_transactions,_deleteTransaction),
+            appBar,
+            Container(
+              height: avaliableHeight * 0.30,
+              child: Chart(_recentTransactions),
+            ),
+            Container(
+              height: avaliableHeight * 0.70,
+              child: TransactionList(_transactions, _deleteTransaction),
+            ),
           ],
         ),
       ),
